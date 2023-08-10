b0b41894d061cadefa4270d9a94674b373d30cad
satyaprakash <satyaprakash101010@gmail.com>
2023-08-10 07:40:06 +0530

    Initial commit

---
+++ kafka-streams-producer/src/main/java/com/example/kafkastreamsproducer/KafkaStreamsProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkastreamsproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamsProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStreamsProducerApplication.class, args);
+    }
+
+}
---
+++ kafka-streams-producer/src/test/java/com/example/kafkastreamsproducer/KafkaStreamsProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkastreamsproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
